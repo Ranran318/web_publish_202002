cdb0b6f2ce5b079ef8e587e2d9e459b4dd25c1be
minjung325 <kimin3258@gmail.com>
2020-04-10 17:58:13 +0900

    java

--- j_javascript_01/js/js_06_for.js
+++ j_javascript_01/js/js_06_for.js
@@ -59,10 +59,69 @@ for(var i = 0; i < 10; i+=1){
 console.log('i가 바깥에서 나올까?', i);
 */
 
+/*
 var i = 0;
 for(; i < 10; i+=1){
   console.log(i);
 
 }
 console.log('i가 바깥에서 나올까?', i);
+*/
+//----------------------------
 
+//var arr = ['월', '화', '수', '목', '금', '토', '일'];
+//console.log(arr[2]);
+//var j;
+//for(j = 0; j < 7 ; j += 1){
+//  console.log( arr[j] + '요일' );
+//
+//---------------------------------------------------
+//var arr = ['월', '화', '수', '목', '금', '토', '일'];
+//console.log(arr);
+//var j;
+//for(j = 0; j < arr.length ; j += 1){
+//  console.log( arr[j] + '요일' );
+//}
+//----------------------------------
+//for(var k in arr){
+//  console.log(arr[k]);
+//}
+//==> 이건 쓰지말고 위에 거 써.
+//-------------------------
+//
+
+//var arr = ['월', '화', '수', '목', '금', '토', '일'];
+//var obj = {
+//  'kf94' : '한국인증 초미세먼지용 마스크'
+//  'kn95' : '중국인증 마스크'
+//  'kf80' : '한국인증 미세먼지용 마스크'
+//};
+
+
+
+
+//var j;
+//for(j = 0; j < arr.length ; j += 1){
+//  console.log( arr[j] + '요일' );
+//--------------------------------------
+//for( var k in obj) {
+//  console.log(obj[k]);
+//}
+//
+//------------------------------------------
+var j;
+for(j = 0; j < arr.length ; j++){
+  console.log( arr[j] + '요일' );
+
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+arr.forEach(function(a){
+  console.log(a)
+});
+
+// for      - 반복(언제든 사용가능)
+// for ~ in - 객체
+// forEach  - 배열
