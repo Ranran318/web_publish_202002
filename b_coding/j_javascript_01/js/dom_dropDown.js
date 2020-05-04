448558aba7efe3fd2136ae7351eb76542cbe164e
Ranran318 <ranran3208@gmail.com>
2020-05-04 12:49:52 +0900

    mod

--- b_coding/j_javascript_01/js/dom_dropDown.js
+++ b_coding/j_javascript_01/js/dom_dropDown.js
@@ -6,7 +6,7 @@ var domDt = document.getElementsByTagName('dt');
 var domDd = document.getElementsByTagName('dd');
 //console.log(domDt);
 
-console.log(domDt.length);
+//console.log(domDt.length);
 
 
 
@@ -20,35 +20,45 @@ for(; i < domDt.length ; i++){
 	domDt[i].setAttribute('tabindex','0');
 }
 
+var DomDdFn = function(action){
+	var j=0;
+	for(; j < domDd.length; j++){
+		domDd[j].style.display = action;
+	}
+};
+
+//DomDdFn=('block');
+
+domDropDown.addEventListener('click', function(){DomDdFn('block');
+	//domDd[0].style.display = 'block';
+	//domDd[1].style.display = 'block';
+	//domDd[2].style.display = 'block';
+	//domDd[3].style.display = 'block';
+	//var j=0;
+	//for(; j< domDd.length; j++){
+	//	domDd[j].style.display = 'block';
+	// }
+}
+)
 
 
+domDropDown.addEventListener('mouseleave', function(){DomDdFn=('none');
 
-domDropDown.addEventListener('mouseenter', function(){
-	domDd[0].style.display = 'block';
-	domDd[1].style.display = 'block';
-	domDd[2].style.display = 'block';
-	domDd[3].style.display = 'block';
+//	domDd[0].style.display = 'none';
+//	domDd[1].style.display = 'none';
+//	domDd[2].style.display = 'none';
+//	domDd[3].style.display = 'none';
+
+//	varj=0;
+//	for(; j<domDd.length; j++){
+//		domDd[j].style.display = 'none';
+//	}
 })
 
-
-domDropDown.addEventListener('click', function(){
-	//domDd[0].style.display = 'click';
-	//domDd[1].style.display = 'click';
-	//domDd[2].style.display = 'click';
-	//domDd[3].style.display = 'click';
-})
-
-domDropDown.addEventListener('mouseleave', function(){
-	domDd[0].style.display = 'none';
-	domDd[1].style.display = 'none';
-	domDd[2].style.display = 'none';
-	domDd[3].style.display = 'none';
-})
-
-domDt[0].addEventListener('focus',function(){
-	domDt[0].style.display = 'block';
-})
+//domDt[0].addEventListener('focus',function(){
+//	domDt[0].style.display = 'none';
+//})
 
 domDt[0].addEventListener('focus', function(){
-	domDd[0].style.display = 'block';
-})
+domDd('block');
+});
