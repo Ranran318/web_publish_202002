7a01366a2245e7bc320ffc3636628471ab99b1c9
Ranran318 <ranran3208@gmail.com>
2020-05-20 12:26:06 +0900

    audio

--- b_coding/b_step_02/js/base/audio.js
+++ b_coding/b_step_02/js/base/audio.js
@@ -4,7 +4,21 @@ var audio = $('.media_music');
 var play = $('.play');
 var pause = $('.pause');
 
+play.on('click', function(e){
+	e.preventDefault();
 
+	//audio.get(0).play();
+audio[0].play();
+	//console.log( audio.get(0))
+//
+});
+
+
+pause.on('click', function(e){
+	e.preventDefault();
+	
+	audio.get(0).pause();
+});
 
 
 })(jQuery);
