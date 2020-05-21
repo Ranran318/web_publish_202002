748f57e63218424b709ec299c543a01841ebc2d8
Ranran318 <ranran3208@gmail.com>
2020-05-21 18:08:20 +0900

    top display

--- 20200520test/js/src/20200520test.js
+++ 20200520test/js/src/20200520test.js
@@ -12,12 +12,14 @@ var headBox = $('#headBox');
 var viewBox = $('#viewBox');
 var viewBoxUl = viewBox.children('ul');
 var viewBoxLi = viewBoxUl.find('li');
-var VLiClone = viewBoxLi.eq(-1).clone(true);
+var VLiClone = viewBoxLi.eq(5).clone(true);
 
 var asideBox = $('#indicatorBox');
 var indiBtn = asideBox.children('div').find('button');
 var prevBtn = indiBtn.find('.prev');
 var nextBtn = indiBtn.find('.next'); 
+
+var topBtn = $('.top_move');
 
 
 //-----
@@ -31,44 +33,59 @@ var nextBtn = indiBtn.find('.next');
 $(document).on('scroll',function(e){
 	var Wscroll= $(document).scrollTop();
 	
-	console.log( Wscroll);
+	//console.log( Wscroll);
 
  if(Wscroll > 45){
 	headBox.css({'width':'100%', 'position':'fixed', 'top':'0'});
  }else{ headBox.css({'position':'absolute','top':'45px'});}
 
 });
-
+ 
 //2.슬라이드 기능 가로스크롤 처리(무한)(광고슬라이드)
 //광고영역에 마우스올릴 시 일시정지, 벗어났을 경우 재생처리(mouseenter, mouseleave)
 
+/*
 viewBoxLi.prepend(VLiClone);
 console.log(VLiClone);
 
 viewBoxLi = viewBox.find('li');
-/*
+
+
+
 viewBox.on('mouseleave', function(e){
 	e.preventDefault();
 	var i = 0
 	var timed = 500
 
-	
-	setInterval(function(){
+	if( viewBoxLi.eq(i) <=6){
+		view
+	}
 
-   
-	},timed,ease);
 });
 
+
 */
-
 /*
-
+*/
 //좌,우 버튼 이전/다음내용 보기 기능
-//현재 광고/전체 광ㅅ고수 표기시(다음내용버튼)
+//현재 광고/전체 광고수 표기시(다음내용버튼)
 //스크롤시 배경이미지 스크롤값보다 약 5% 더 빠르게 배경이미지 상단으로 이동처리
-//top.move버튼 스크롤 300이하일 경우 사라지고, 이후 나타나는 기능(300ms)
-//전체 focus 처리시 배경색 #ff0 색상처리
 
+//top.move버튼 스크롤 300이하일 경우 사라지고, 이후 나타나는 기능(300ms)
+
+
+$(document).on('scroll',function(e){
+	var Wscroll= $(document).scrollTop();
+	
+	//console.log( Wscroll);
+
+ if(Wscroll > 300, topBtn.css('display') === 'none') {
+	topBtn.css({'display':'block'})};
+
+ });
+
+//전체 focus 처리시 배경색 #ff0 색상처리
+/*
 var timed = 500;
 
 $(viewBox).on('mouseleave',function(e){
@@ -80,8 +97,8 @@ $(viewBox).on('mouseleave',function(e){
 
 
 });
-*/
 
+*/
 
 
 //setInterval
