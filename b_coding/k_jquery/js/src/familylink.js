47e49c61917faeaaf55c11527bdfe8a105df0326
unknown <ranran3208@gmail.com>
2020-07-02 11:15:47 +0900

    remind

--- b_coding/k_jquery/js/src/familylink.js
+++ b_coding/k_jquery/js/src/familylink.js
@@ -4,6 +4,7 @@
 
  var select = $('#linkpage');
  select.on('change', function(){
+	      e.preventDefault();
 				var val = $(this).val();
 				//console.log(val);
         window.location = val;
@@ -11,12 +12,12 @@
  
 	// -----------------------------------------
 	var t2 = $('.type_02');
-	var t2dt = t2.find('dt');
+	var t2Dt = t2.find('dt');
 
-	t2dt.on('click', function(e){
-		t2.finde('dd').show();
+	t2Dt.on('click', function(e){
+		t2.find('dd').show();
 	});
-
+//===========================================
 	var t3 = $('.type_03');
 	var t3Dt = t3.find('dt');
 	var t3Dd = t3.find('dd');
@@ -24,14 +25,16 @@
 
 	t3Dt.on('click', function(e){
 			 e.preventDefault();
-			 t3.find('dd').stop().slideToggle(100);
+			 t3Dd.stop().slideToggle(100);
    		});
 	 
-			 t3Dd.on('click', function(e){
+			 t3Dd.find('button').on('click', function(e){
 						e.preventDefault();
 						var dataHref = $(this).attr('data-href');
+						var dataHref = $(this).attr('data-link');
+						t3Dd.stop().slideToggle(100);
 						//console.log(dataHref);
-						go.attr({'href':dataHref, 'arget':'blank'});
+						go.attr({'href':dataHref, 'target':'blank'});
 						go.focus();
 			 });
 
