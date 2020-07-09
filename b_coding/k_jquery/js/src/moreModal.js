be6398e71f6bdb2dbcbabd234bf2066db836f3b0
Ranran318 <ranran3208@gmail.com>
2020-07-09 15:54:06 +0900

    PR

--- b_coding/k_jquery/js/src/moreModal.js
+++ b_coding/k_jquery/js/src/moreModal.js
@@ -1,28 +1,37 @@
 //moreModal.js
-var listView;
+// var listView;
 var colorArr = [];
+var NowListLen;
+var listData; 
 
 (function($){
 		listView = $('#listViewBox');
 		var listUl = listView.find('ul');
 		var moreBtn = listView.find('.more_btn');
-		var listData;
+		//var listData;
 		var liCode = '<li><a href="#"><span></span></a></li>';
 
 		//console.log('before');
-		var i = 0;
+		i = 0;
 
 		var InsertLi = function(db, color){
-			 var NowListLen = i + 3;
+			    NowListLen = i + 3;
 			 
-			 for(; i<NowListLen; i+=1){
-							 listUl.append(liCode);
-							 //console.log(i);
-							 listUl.find('li').eq(i).children('a').css({
-								  backgroundColor:color[i]
-							 });
-							 listUl.find('li').eq(i).find('span').text(db[i].displayName);
-			 }
+	       for(; i<NowListLen; i+=1){
+					       console.log(i, db.length)
+                 if(i >= db.length){
+			  						 moreBtn.hide();
+			  						 break;		
+			  				}else{
+			  	 
+			  				 listUl.append(liCode);
+			  				 //console.log(i);
+			  				 listUl.find('li').eq(i).children('a').css({
+			  					  backgroundColor:color[i]
+			  				 });
+								 listUl.find('li').eq(i).find('span').text(db[i].displayName);
+								}
+			   }
 		};
     
 
@@ -30,24 +39,21 @@ var colorArr = [];
 				url:'../data/people2.json',
 				async:false,
 				success:function(data){
-								listData = data;
-								// 컬러 차트 만들기 ------------------------------------------------------
-								var dataLen = listData.length;
-								colorArr = [];
-								for(var colorI = 0; colorI < dataLen; colorI += 1){
-									      //hsla(각도, 50%, 50%, 1 ) ; 색상, 채도 , 명도, 투명도
-                         colorArr[colorI] = 'hsla(' + parseInt(Math.random()*360) + ', '+ '50%, 50%, 1)';
-								}
-								 //console.log(colorArr);
-								 //---------------------------------------------------
+						listData = data;
+						// 컬러 차트 만들기 ------------------------------------------------------
+						var dataLen = listData.length;
+						//var colorArr = [];
+						for(var colorI = 0; colorI < dataLen; colorI += 1){
+										//hsla(각도, 50%, 50%, 1 ) ; 색상, 채도 , 명도, 투명도
+											colorArr[colorI] = 'hsla(' + parseInt(Math.random()*360) + ', '+ '50%, 50%, 1)';
+						}
+							//console.log(colorArr);
+							//더보기 버튼 클릭---------------------------------------------------
+						InsertLi(listData, colorArr);
+						moreBtn.on('click', function(){
 								InsertLi(listData, colorArr);
-
-								moreBtn.on('click', function(){
-
-								 InsertLi(listData, colorArr);
-								});
-							}
-
+						});
+					}
 		});
 
 
--- b_coding/k_jquery/js/src/popupModal.js
+++ b_coding/k_jquery/js/src/popupModal.js
@@ -2,11 +2,13 @@
 
 (function($){
 
-	 var cardData = listView;
+	 var cardData = listData;
+
 	 var colorData = colorArr;
-	 var listView = $('#listViewBox');
-	 var listUl = listView.find('ul');
+	 var listViewBox = $('#listViewBox');
+	 var listUl = listViewBox.find('ul');
 	 var modalWin = $('#modalBox');
+	 var modalView = modalWin.find('.modal_view');
 	 var modalImg = modalWin.find('.modal_image');
 	 var modalCloseBtn = modalWin.find('.modal_close');
 
@@ -16,76 +18,84 @@
 	 //이를 해결하기 위한 방법으로는, 
 	 //첫번째 : $(document) 에서부터 찾아서 선택하는 형식을 취하면 된다.
 	 //두번째 : 최종선택자는 클릭 후에 판단하는 형식으로 취한다.
+	 var cardI;
 	 
-
-	 $(document).find(listUl).on('click', 'li',function(e){
+	 $(document).find(listUl).on('click', 'li', function(e){
 		 e.preventDefault();
-		 var indexCard = $(this).index();
-		 console.log(indexCard);
 		 
-		 modalImg.css({
-			 backgroundImage:'URL('+ cardData[indexCard].image + ')',
-			 backgroundSize:'cover',
-			 backgroundRepeat:'no-repeat',
-			 backgroundPosition: '50% 50%'
-	  });
+		 var indexCard = $(this).index();
+		 cardI = indexCard;
+		//  console.log(cardData[indexCard].image);
+		var modalImgSource = cardData[indexCard].image;
+			modalImg.css({ backgroundImage:'URL(' + modalImgSource + ')',  backgroundSize:'cover', backgroundRepeat:'no-repeat', backgroundPosition: '50% 50%' });
+		
+			
 		 modalImg.text(cardData[indexCard].model_);
-		 modalWin.fadeIn(function(){
-			     $(this).on('mousemove', function(e){
-						     var mouseP = { x = e.clientX, y:e.clientY};
-								 console.log(mouseP);
-								 modalImg.css({transform:'rotateX('+  mouseP.x / 100*3 + 'deg) rotateY('+ mouseP.X / 100*10 + 'deg)'
-								});
-					 });
+		 modalWin.fadeIn(function(){});
+	});
 
-					 //keyboard 이벤트
-					 $(document).on('keyup',function(e){
-						 var key = e.key;
-						 var keycase = key.toLowerCase();
-						 var keyTrue = true;
-						 if(keyTrue){
-							  kyeTrue = false;
+
+
+	 // 마우스 움직일 경우
+	 modalView.on('mousemove', function(e){
+		var mouseP = { x: e.clientX, y: e.clientY};
+		//console.log(mouseP);
+		modalView.css({transform:'rotateX('+  -mouseP.x / 100*2 + 'deg)\
+									translate('+ mouseP.y / 100*10 + 'px)\
+									perspective(500px)'});
+	});
+
+
+
+ //키보드 처리로 인한 이미지 변경
+		//keyboard 이벤트
+		
+		$(document).on('keyup',function(e){
+			var key = e.key;
+			var keycase = key.toLowerCase();
+			
+			switch(keycase){
+						 case 'arrowup'  :
+						 case 'arrowleft' :
+									cardI -= 1;
+						 break;
+
+						 case 'arrowdown' : 
+						 case 'arrowleft':
+							cardI += 1;
+						 break;
+
+						 case 'escape' : 
+									modalWin.fadeOut();
+									listUl.find('li').eq(cardI).find('a').focus();
+						 break;
+					 
+						 }
+
+						 console.log(cardI);
+						 if(cardI <= 0){
+							      cardI = 0;
+						 }else if(cardI >= NowListLen){
+						      	cardI = NowListLen;
+										modalWin.fadeOut();
+						 }else{
+							 modalImg.css({backgroundImage:'URL('+ cardData[cardI].image + ')'});
+						}
 						 
+			 
+						 
+			//console.log(key);
+ });
 
-						 console.log(keycase);
-						 switch(keycase){
-										case 'arrowup'  :
-										case 'arrowleft' :
-                         indexCard -= 1;
-										break;
-
-										case 'arrowdown' : 
-										case 'arrowleft':
-											indexCard += 1;
-										break;
-
-										case 'esc' : 
-										     modalWin.fadOut();
-										     listUl.find('li').eq(indexCard).find('a').focus();
-										break;
-									
-										}
-
-										console.log(indexCard);
-										if(indexCard <= 0){
-											indexCard = 0;
-										}else if(indexCard >= NowListLen){
-											     indexCard = NowListLen;
-													 modalWin.fadeOut();
-										}else{
-											modalImg.css({backgroundImage:'URL('+ cardData[indexCard].image + ')'});
-										}
-										
-							}
-                    
-						 //console.log(key);
-				});
-		 });
-	 });
+ //닫기버튼 
 
 	 modalCloseBtn.on('click', function(e){
 	  	e.preventDefault();
 			modalWin.fadeOut(100);
 	 });
+
+
+
+	 
 	   
 })(jQuery);
