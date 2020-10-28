61320fd5b160638061f56bc2884bbed4424a70db
unknown <ranran3208@gmail.com>
2020-10-28 19:40:13 +0900

    캔버스 직사각형

--- b_coding/j_javascript_01/js/src/canvas_01.js
+++ b_coding/j_javascript_01/js/src/canvas_01.js
@@ -1,7 +1,7 @@
 //canvas_01.js
 
 (function($){
-		 //var canvas = $('.paper');
+     //var canvas = $('.paper');
      //var canvas = document.getElementsByClassName('paper')[0];
      var canvas = document.querySelector('.paper');
 		 //if (canvas.getContext){ console.log('canvas 지원 브라우저 ');}
@@ -10,16 +10,17 @@
 		 //ctx.fillRect(x, y, width, height); -> 면을 채우는 사각형
 		 //ctx.clearRect(x, y, width, height); -> 채워진 사각형 구멍을 뚫어주는 형태
 		 //ctx.strokeRect(x, y, width, height); -> 선형태의 사각형;
-		 ctx.fillStyle = "#ff0";
-		 ctx.strokesyale="#fff";
-		 ctx.lineWidth = 10;
-     ctx.fillRect(100, 100, 600, 600); 
-     ctx.clearRect(150, 150, 400, 400);
-     //ctx.fillRect(170, 170, 300, 300);
-		 //ctx.clearRect(172, 172, 296, 296);
-		 ctx.strokeRect(170,1770,300,300);
+		 ctx.fillStyle   = "#ff0";
+		 ctx.strokesyale = "#fff";
+		 ctx.lineWidth   = 10;
 
-			ctx.fillStyle = "#fff";
-			ctx.fillRect(200,200,100,100);
+         ctx.fillRect(100, 100, 600, 600); 
+         ctx.clearRect(150, 150, 400, 400);
+         //ctx.fillRect(170, 170, 300, 300);
+		 //ctx.clearRect(172, 172, 296, 296);
+		 ctx.strokeRect(170, 1770, 300, 300);
+
+		 ctx.fillStyle = "#fff";
+		 ctx.fillRect(200, 200, 100, 100);
 
 		})(jQuery);
