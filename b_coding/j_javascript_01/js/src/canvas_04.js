0d4fac22abb9bf43fe2babf503d4b2ba85172e09
unknown <ranran3208@gmail.com>
2020-10-28 21:06:10 +0900

    캔버스 이미지 이동..안 됨

--- b_coding/j_javascript_01/js/src/canvas_04.js
+++ b_coding/j_javascript_01/js/src/canvas_04.js
@@ -3,14 +3,14 @@
 var canvas = document.querySelector('.paper');
 var ctx = canvas.getContext('2d');
 //캔버스를 위한 무조건 적인 세팅. (function(){})(jQuery); 같은.
-ctx.fillStyle = "#579";
-
+////ctx.fillStyle = "#579"; ============ 첫번째 버전 - 에러
+ctx.fillStyle = 'hsla('+ Math.random() * 360 +', 100%, 50%, 0.7)';
 var MoveCircle = function(posX, poY){
 
 	ctx.clearRect(0,0, canvas.width, canvas.height);
 //	ctx.fillStyle = 'hsla('+ Math.random()*360 +', 100%, 50%, 0.7');
 	ctx.beginPath();
-	ctx.arc(posX, poY, 80, 0, Math.PI * 2, false);
+	ctx.arc(posX, poY, 80, 0, Math.PI*2, false);
 	ctx.closePath();
 	ctx.fill();//면을 채우는 효과
 };
@@ -19,24 +19,21 @@ var MoveCircle = function(posX, poY){
 	
 
 
-var i = 0
+var i = 100;
 var j = 400;
 var setmove;
 var MoveAni = function(){
      setmove = setInterval(function(){
               	i += 20;
-              	if(i > 200){
-              		 j += 10;
-              		 if(j > 600){
-              			  j = 600;			 
-              		 }
-              	}
-              	MoveCircle(i,j);
-								//console.log(i);
-      if(i > 700){
-				clearInterval(setmove);
-			}          
-    },100);
+              	if(i > 200){ j += 10;
+              		  if(j > 600){ j = 600;}
+              	};
+              	MoveCircle(i, j);
+				//console.log(i);
+                if(i > 700){
+				      clearInterval(setmove);
+			    };   
+    }, 40);
 };
 
 //MoveAni();
@@ -45,13 +42,13 @@ var k = 0;
 var reqAni;
 function Move(){
 	 k += 1;
-	 console.log(k);
+	 //console.log(k);
 	 reqAni = requestAnimationFrame(Move);
 	 
 	 if(k >= 500){
 		      cancelAnimationFrame(reqAni);
 	 }
-	}
+}
 
 Move();
 
